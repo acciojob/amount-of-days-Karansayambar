126abb65feb6883bad5f62cda7c6dd4d4de7b5bb
Karan Sayambar <86702398+Karansayambar@users.noreply.github.com>
2023-12-10 12:06:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+
+function daysOfAYear(year){
+	if(year % 4 == 0){
+		if((year%4 == 0 && year%100 != 0) || year%400==0){
+			return 366;
+		}
+	}else{
+		return 365;
+	}
+}
+let res = daysOfAYear(2022);
+console.log(res);
